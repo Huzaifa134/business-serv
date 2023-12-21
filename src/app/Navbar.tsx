a4bd610df6fb03dac901d55de8eac3c64e677c4b
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2023-12-22 01:55:06 +0500

    nabar implemented

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* Header */}
+      <header className="flex justify-between px-32 py-4 bg-gray-100 items-center">
+        {/* Left Side */}
+        <div>
+          <h1 className="text-3xl font-bold text-gray-600">Business</h1>
+        </div>
+
+        {/* Right Side */}
+        <nav>
+          <ul className="text-lg font-medium hidden md:flex gap-x-4 text-gray-600">
+            <li>
+              <Link href={"/"}>Home </Link>
+            </li>
+            <li>
+              <Link href={"/"}>Services</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Contact</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Buy Now</Link>
+            </li>
+          </ul>
+          <div className="flex md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Menu />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <ul className="text-lg font-medium gap-x-4 text-gray-600">
+                    <li className="py-3">
+                      <Link href={"/"}>Home </Link>
+                    </li>
+                    <li className="py-3">
+                      <Link href={"/"}>Services</Link>
+                    </li>
+                    <li className="py-3">
+                      <Link href={"/"}>Contact</Link>
+                    </li>
+                    <li className="py-3">
+                      <Link href={"/"}>Buy Now</Link>
+                    </li>
+                  </ul>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "../app/Navbar";
 
 export default function Home() {
   return (
     <>
-      <h1>Business Website</h1>
+      {/* <h1>Business Website</h1> */}
+      <Navbar />
     </>
   );
 }
