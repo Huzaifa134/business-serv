1ba1f2964ed8eacb687bb268789c275d924c3051
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2023-12-23 02:18:29 +0500

    header done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "../app/Navbar";
+import HomePage from "../app/home/Home";
 
 export default function Home() {
   return (
     <>
       {/* <h1>Business Website</h1> */}
       <Navbar />
+      <HomePage />
     </>
   );
 }
