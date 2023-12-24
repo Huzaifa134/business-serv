aa4bc85ea5f2aecfd29898a4917f44381317d503
dynamomannal <mannalrana1999@gmail.com>
2023-12-24 23:10:07 +0500

    manal

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -13,17 +13,17 @@ const Home = () => {
         backgroundImage:`url('/Background.png')`,
         height:"700px",
       }
-    }>
-      {/* <h1>Home component</h1> */}
+    }> 
+     {/* <h1>Home component</h1> */}
       <div className="flex items-center w-full justify-center lg:gap-x-60 mt-100 md:gap-x-10 flex-row md:px-20 max-[717px]:gap-x-5 max-[717px]:flex-col ">
-        <div className="my-0 max-[717px]:my-5 max-[717px]:flex max-[717px]:flex-col max-[717px]:justify-content">
-          <h1 className="font-bold lg:text-3xl md:text-3xl max-[717px]:text-[60px] max-[717px]:tracking-widest max-[717px]:text-center ">
+        <div className="my-0 max-[717px]:my-5 max-[717px]:flex max-[717px]:flex-col max-[717px]:justify-center">
+          <h1 className="font-bold lg:text-5xl md:text-3xl max-[717px]:text-[60px] max-[717px]:tracking-widest max-[717px]:text-center ">
             Business Consultant 
           </h1>
-          <p className="lg:text-base md:text-sm sm:text-[15px] max-[717px]:text-center">
+          <p className="lg:text-base text-center md:text-sm sm:text-[15px] max-[717px]:text-center">
             Grow your business
           </p>
-          <div className="  ">
+          <div className=" flex flex-col items-center  ">
             <button className="bg-sky-500 hover:bg-sky-700 py-3 px-5 rounded-xl my-10 text-[#feffff] ">
               Save changes
             </button>
