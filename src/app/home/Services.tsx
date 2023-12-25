b67ce90577d17fd9e0db6262d1957525d866e265
dynamomannal <mannalrana1999@gmail.com>
2023-12-26 02:23:09 +0500

    First manal

--- src/app/home/Services.tsx
+++ src/app/home/Services.tsx
@@ -41,7 +41,7 @@ export default function Example() {
   return (
     <div className=" py-24 sm:py-32 ">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h1 className="text-5xl  font-bold text-black text-center">
+        <h1 className="text-5xl  font-bold text-center">
           Our Services
         </h1>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16   pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
