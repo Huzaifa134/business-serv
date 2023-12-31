7adcfac49577ad065a86b39b6e9a2500dcac15cf
dynamomanal <manalrana1999@gmail.com>
2023-12-31 05:15:15 +0500

    First Commit

--- src/components/gooeybutton.tsx
+++ src/components/gooeybutton.tsx
@@ -1,14 +1,14 @@
-const GooeyButton = () => {
-    return (
-      <>
-       
-    <button className="relative inline-block p-4 border-2 border-gray-300 rounded-lg overflow-hidden z-10 bg-white font-bold text-blue-500 transition-all duration-1200 ease-linear cursor-pointer hover:text-white hover:bg-blue-500 hover:border-blue-500">
-      <span className="button-content">Click me</span>
-      <div className="absolute after-gradient-spin right-[-112px] bottom-[-38px] w-24 h-24 animate-spin duration-1800"></div>
-    </button>
-  );
+const GooeyButton = () => (
+  <>
 
-</>
-    )
-       }
+    <button className=" w-75 relative inline-block p-4 px-6 mt-10 border-2 border-blue-300 rounded-lg overflow-hidden z-10 bg-gradient-to-br from-white via-blue-400 to-cyan-500 font-bold text-white hover:text-black hover:bg-transparent">
+          <span className="button-content"> 
+          Start Free Trial 
+           </span>
+        
+        </button>
+
+
+  </>
+)
 export default GooeyButton;
