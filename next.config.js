a21723a61d41aeb1fde04259cc21801727ce1333
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-02-04 03:27:21 +0500

    config file

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
 
 module.exports = {
     images: {
-      domains: ['images.unsplash.com'],
+      domains: ['images.unsplash.com','https://www.pacificfh.com/'],
     },
   }
 // next.config.js
