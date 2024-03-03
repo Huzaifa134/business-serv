83e9c67e34b58e6939c1c56f7faff182e86b9ace
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-03-04 00:14:30 +0500

    guide

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,24 +1,38 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { client } from "../../../sanity/lib/client";
 import { PortableText } from "@portabletext/react";
-
 async function getData() {
   const fetchData = await client.fetch(`*[_type=='aboutus']`);
 
   return fetchData;
 }
 
-export default async function AboutUs() {
+export default async function getStaticProps() {
   const data = await getData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const result = await client.fetch(`*[_type=='aboutus']`);
+  //       setData(result);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   }
+
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="flex flex-col mx-20 px-5">
       {data?.map((data: any, i: number) => (
-        <article key={i} className=" items-center ">
+        <article key={i} className=" items-center pb-14">
           {/* <ul>
           <li className="mx-20 "> */}
 
-          <div className="font-bold text-center md:text-xl max-[900px]:text-[20px] max-[700px] my-18">
+          <div className="font-bold text-center md:text-xl max-[900px]:text-[20px] max-[700px] my-18 p-10">
             {" "}
             {data?.title}{" "}
           </div>
