5ba4cc0e9e3fa6d73a6b630cb3297269714d801c
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-03-04 00:33:50 +0500

    guides

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -3,27 +3,21 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { client } from "../../../sanity/lib/client";
 import { PortableText } from "@portabletext/react";
-async function getData() {
-  const fetchData = await client.fetch(`*[_type=='aboutus']`);
 
-  return fetchData;
-}
+export default function AboutUs() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await client.fetch(`*[_type=='aboutus']`);
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
 
-export default async function getStaticProps() {
-  const data = await getData();
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const result = await client.fetch(`*[_type=='aboutus']`);
-  //       setData(result);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   }
-
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
     <div className="flex flex-col mx-20 px-5">
